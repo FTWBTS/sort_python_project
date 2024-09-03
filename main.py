a7e6f1de460efdda9fb27c0266c769768e1f41ac
FTWBTS <guhailong913@gmail.com>
2024-09-03 17:55:46 +0800

    testhale

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('这是各类排序')
     arr = [64, 34, 25, 12, 22, 11, 90]
-    print("Bubble Sort:", BubbleSort.bubble_sort(arr))
-    print("Insertion Sort:", InsertionSort.insertion_sort(arr))
-    print("Quick Sort:", QuickSort.quick_sort(arr))
-    print("Selection Sort:", SelectionSort.selection_sort(arr))
     print("Merge Sort:", MergeSort.merge_sort(arr))
+    print("Bubble Sort:", BubbleSort.bubble_sort(arr))
+    print("Quick Sort:", QuickSort.quick_sort(arr))
+    print("Insertion Sort:", InsertionSort.insertion_sort(arr))
+    print("Selection Sort:", SelectionSort.selection_sort(arr))
